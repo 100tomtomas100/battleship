32d3f7451b1444a5177702a183ff27d7bef7464a
Tomas Pocius <pocius.tomastp@gmail.com>
2022-09-22 09:59:01 +0300

    added squares, computer can position the ships randomly, its possible to attack  the sips, red color for hitting the ship, black collor for missing the target

--- src/script.js
+++ src/script.js
@@ -1,63 +1,71 @@
 // const parse = require("./style.css");
 // import sheet from './style.css' assert { type: 'css' };
+
+// create a board on the screen;
+
+const screenBoard = () => {
+  let container = document.getElementById("place-board");
+  container.style.gridTemplateColumns = "repeat(10, auto)";
+  for (let i = 1; i <= 10*10; i++) {    
+    let square = container.appendChild(document.createElement("div"));
+    square.id = i;
+    square.className = "square";
+    square.style.border = "thin solid black";
+  }
+};
+
 // construct new ship
-const shipFactory = (coor) => {
+
+const shipFactory = (coor, blSq) => {
   let shipCoor = coor;
   let hit = [];
   let sunk = false;
-  return {shipCoor, hit, sunk} 
+  let sunkBl = blSq;
+  for(let i = 0; shipCoor.length > i; i++){
+    let colorShip = document.getElementById(coor[i]);
+    colorShip.style.backgroundColor = "black";
+  }
+  return {shipCoor, hit, sunk, sunkBl} 
 };
 
 // make and update the game board
+
 const gameBoard = () => {  
   let ships = [];
-  const coorAll = () => {
-    for (let i = 0; i < 11*11; i++) {
-      let square = container.appendChild(document.createElement("div"));
-      square.id = i;
-    }
-  };
-  const createShip = (coor) => {
-    ships.push(shipFactory(coor));
+  const createShip = (coor, blSq) => {
+    ships.push(shipFactory(coor, blSq));
   };
   const missedAttack = [];
-  const receiveAttack = (shot, s = ships) => {
+  const receiveAttack = (shot, s = ships) => {    
     let shipHit = false;
     let checkShips = (ship) => {
-      for (let j = 0; ship.shipCoor.length > j; j++) {
-        if (ship.shipCoor[j] === shot){          
+      for (let j = 0; ship.shipCoor.length > j; j++) {              
+        if (ship.shipCoor[j] === shot){         
           ship.hit.push(shot);         
           shipHit = true;
-        } 
+        }; 
         if (ship.hit.length === ship.shipCoor.length) {
           ship.sunk = true;
-        } 
-      }
-    }
+        }; 
+      };
+    };
     for (let i = 0; ships.length > i; i++) {
       checkShips(ships[i]);
     }
     if(shipHit === false) {
       missedAttack.push(shot)
+      return "missed"
     }
+    
+    
   };
-  // console.log(ships)
-  // console.log(missedAttack)
-
-  return {createShip, receiveAttack, ships}
+  return {createShip, receiveAttack, ships, missedAttack}
 };
-// let dur = gameBoard();
-// dur.createShip(["m2","m3","m4"])
-// gameBoard().createShip(["m6","m7","m8"])
-// // gameBoard().receiveAttack("m8");
-// // gameBoard().receiveAttack("m6");
-// dur.receiveAttack("m4");
-// dur.receiveAttack("m9");
-// dur.receiveAttack("m10");
-
-const player = () => {
+// create a new player
+const player = (name) => {
   
   // all coordinates as well available coordinates
+
   let allCoo = (() => {
       let result = [];
       for(let i = 1; 101 > i; i++) {
@@ -65,7 +73,9 @@ const player = () => {
       }
       return result;
   })();
+
   // remove coordinates from available
+
   let removeCoo = (remove) => {      
       for(let i = 0; remove.length > i; i++) {      
         let result = allCoo.filter(nr => nr != remove[i]);
@@ -74,8 +84,11 @@ const player = () => {
   };
   
 // place ships on the board randomly 
-  const shipPlace = (length) => {   
+
+  const shipPlace = (length) => { 
+
     // ship to be placed horizontal or vertical 
+
     const placement = (() => {
       let random = Math.round(Math.random());   
       if (random === 0) {
@@ -84,7 +97,9 @@ const player = () => {
         return "horizontal"
       }
     })();   
+
     // get available coordinates to position the ships
+
     const shipCoo = () => {
       let randomCoo = allCoo[Math.round((Math.random())*allCoo.length)];
       let coo = [randomCoo];   
@@ -106,12 +121,12 @@ const player = () => {
           };
         };        
         if(availability.length === length 
-          && availability[0] < 10 && placement === "horizontal" 
+          && availability[0] <= 10 && placement === "horizontal" 
           && availability[availability.length - 1] > 10) {
             availability = [];
         } else if (availability.length === length 
           && availability[0] > 10 && placement === "horizontal" 
-          && availability[0].toString()[0] != availability[availability.length - 1].toString()[0]) {
+          && (availability[0] - 1).toString()[0] != availability[availability.length - 1].toString()[0]) {
             availability = [];               
         } 
       })();
@@ -121,28 +136,134 @@ const player = () => {
         return shipCoo(length);
       }      
     };
+    let coo = shipCoo();
+
+    // a square around a ship to be blocked from creating new ships
+    
+    const blockSq = (() => { 
+      let block = [];     
+      if(placement === "horizontal") {
+        for (let i = 0; coo.length > i; i++) { 
+          if(i === 0) {
+            let block1 = coo[i] - 1;
+            let block2 = coo[i] - 10;
+            let block3 = coo[i] + 10;
+            let block9 = coo[i] - 11;
+            let block10 = coo[i] + 9;
+            if(coo.length === 1) {              
+              let block11 = coo[i] -9;
+              let block12 = coo[i] + 1;
+              let block13 = coo[i] + 11;
+              block.push(block11, block12, block13);
+            }
+            block.push(block1, block2, block3, block9, block10);
+          } else if (i === coo.length - 1) {
+            let block6 = coo[i] - 10;
+            let block7 = coo[i] + 1;
+            let block8 = coo[i] + 10;
+            let block14 = coo[i] - 9;
+            let block15 = coo[i] + 11;
+            block.push(block6, block7, block8, block14, block15);
+          } else if (i > 0) {
+            let block4 = coo[i] - 10;
+            let block5 = coo[i] + 10;
+            block.push(block4, block5);
+          } 
+        }
+      } else if (placement === "vertical") {
+        for (let i = 0; coo.length > i; i++) {
+          if(i === 0) {
+            let block1 = coo[i] - 1;
+            let block2 = coo[i] - 10;
+            let block3 = coo[i] + 1;
+            let block9 = coo[i] - 11;
+            let block10 = coo[i] - 9;
+            if(coo.length === 1) {
+              let block11 = coo[i] + 9;
+              let block12 = coo[i] + 10;
+              let block13 = coo[i] + 11;
+              block.push(block11, block12, block13)
+            }
+            block.push(block1, block2, block3, block9, block10);
+          } else if (i === coo.length -1) {
+            let block6 = coo[i] - 1;
+            let block7 = coo[i] + 1;
+            let block8 = coo[i] + 10;
+            let block14 = coo[i] + 9;
+            let block15 = coo[i] + 11
+            block.push(block6, block7, block8, block14, block15);
+          } else if (i > 0) {
+            let block4 = coo[i] - 1;
+            let block5 = coo[i] + 1;
+            block.push(block4, block5);
+          }
+        }
+      }
+      let blockAvail = block.filter(nr => nr > 0 && nr < 101);
+      removeCoo(blockAvail);
+      return blockAvail;
+    })();
+
     // construct the ships with given coordinates
-    const constructShip = (() => {
-      let coo = shipCoo();          
-      playerBoard.createShip(coo);
+
+    const constructShip = (() => {    
+      name.createShip(coo, blockSq);
       removeCoo(coo);      
-    })();    
-  }; 
-  shipPlace(3);
-  shipPlace(4);
-  shipPlace(2);
-  shipPlace(4);
-  
-  console.log(allCoo)
-  
+    })();
+  };   
+  return {shipPlace}
+};
+
+//computer random positioning and play
+
+const VScomputer = () => {
+  const screen = screenBoard();
+  const playerBoard = gameBoard();
+  const computer = player(playerBoard);
+  const shipPlacement = (() => {
+    for (let i = 1; 11 > i; i++) {
+        if (i < 2) {
+          computer.shipPlace(4);
+        } else if (i < 4) {
+          computer.shipPlace(3);
+        } else if (i < 7) {
+          computer.shipPlace(2);
+        } else if (i < 11) {
+          computer.shipPlace(1);
+        };
+    };
+  })();  
+  console.log(playerBoard.ships)
+  return {playerBoard}
 }
-const playerBoard = gameBoard();
-const computer = player();
-playerBoard.receiveAttack(3)
+const enemy = VScomputer()
+
+// shoot the enemy 
+
+const shoot = (() => {
+  let square = document.querySelectorAll(".square"); 
+  square.forEach(sq => {
+    sq.addEventListener("click", () => {
+      let shot = enemy.playerBoard.receiveAttack(Number(sq.id));      
+      let text = document.createElement("p");
+      sq.appendChild(text);
+      text.style.position = "absolute";
+      console.log(shot)
+      if(shot === "missed"){
+        text.innerHTML = "&#x2716;";
+      } else {
+        text.innerHTML = "&#x274C;";
+        text.style.fontSize = "150%"
+      }
+      
+      
+      
+  
+    });
+  })
+})();
 
 
-
-console.log(playerBoard.ships)
 
 // const shipFactory = (length) => {
 //   return length;
